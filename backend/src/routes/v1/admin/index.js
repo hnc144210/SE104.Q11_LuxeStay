82c874ad739253b1d82ee9e8d4ac1bf73cc7cfc6
Pham Viet Duc <ducp07052@gmail.com>
2025-12-06 21:40:27 +0700

    update

--- backend/src/routes/v1/admin/index.js
+++ backend/src/routes/v1/admin/index.js
@@ -8,7 +8,7 @@ const bookingController = require("../../../controllers/bookingController");
 // Import file routes con (Đảm bảo file này nằm cùng thư mục admin)
 const staffRoutes = require("./staffRoutes");
 const roomRoutes = require("./roomRoutes");
-
+const customerRoutes = require("../staff/customerRoutes");
 // --- CHỐT CHẶN BẢO MẬT ---
 // Chỉ cần khai báo 1 lần ở đây, tất cả con bên dưới đều được hưởng
 router.use(authenticate);
@@ -17,7 +17,7 @@ router.use(authorize("admin"));
 // --- ĐỊNH NGHĨA ---
 router.use("/staffs", staffRoutes); // -> /api/v1/admin/staffs
 router.use("/rooms", roomRoutes); // -> /api/v1/admin/rooms
-
+router.use("/customers", customerRoutes); // -> /api/v1/admin/customers
 // Booking routes
 router.get("/bookings", bookingController.getBookingsForStaffAdmin);
 router.delete("/bookings/:id", bookingController.cancelBookingByStaffAdmin);
