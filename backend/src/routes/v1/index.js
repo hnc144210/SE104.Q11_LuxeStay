68f4062a66e682bf21f8b40ae32efbf54d01448c
Pham Viet Duc <ducp07052@gmail.com>
2025-12-06 21:40:27 +0700

    update

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -17,7 +17,6 @@ router.use("/auth", authRoutes);
 router.use("/rooms", roomRoutes);
 router.use("/bookings", bookingRoutes);
 
-router.use("/customer", customerRoutes); // giữ của bạn
 router.use("/admin", authenticate, adminRoutes); // bảo mật
 // router.use("/staff", authenticate, staffRoutes); // bảo mật
 
